dc8cf09ee47c18029942c86a042cb0a445b73633
Jan Hybl <jan.hybl@broadcom.com>
2025-01-28 15:36:45 +0100

    try fs zen
    Signed-off-by: Jan Hybl <jan.hybl@broadcom.com>

---
+++ packages/zowe-explorer/src/web/polyfills/fs-zen.js
@@ -0,0 +1,9 @@
+import { fs } from "@zenfs/core";
+export default fs;
+// const { fs: fsZen } = require("@zenfs/core");
+
+// const { WebStorage } = require("@zenfs/dom");
+
+// configure({ backend: WebStorage, storage: localStorage });
+
+// module.exports = fsZen;
